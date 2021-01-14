0b65d3cf25abe50b0fdef2944c9d9bb5f583311d
Adina Sames <adina.sames@gmail.com>
2021-01-14 17:45:20 +0200

    tema curs js 3

---
+++ bucle/bucle.js
@@ -0,0 +1,23 @@
+for (let j = 0; j < 21; j++) {
+   document.write("#")
+    
+}
+for (let k = 0; k < 10; k++) {
+    document.write("#","<br>", "#")
+for (let i = 0; i < 20; i++) {
+    document.write(i * 0)
+
+    
+}
+}
+
+for (let l = 0; l < 1; l++) {
+    document.write("#")
+
+}
+
+document.write("<br>")
+
+for (let m = 0; m < 22; m++) {
+    document.write("#")
+}
