26b4f25b4c99002b59afefbe00f783c67e9c9625
Adina Sames <adina.sames@gmail.com>
2021-02-12 11:04:02 +0200

    tema grid

---
+++ 50 coding challenges/challenges.js
@@ -0,0 +1,35 @@
+// 1. Display numbers from 1 to 10 - fara defer la script in html
+
+// for (let i = 1; i < 11; i++) {
+//     document.write(i + "<br>")
+// }
+
+
+// 2. Display the odd numbers less than 100
+
+// for (let i = 1; i < 100; i+=2) {
+//     document.write(i + "<br>")
+// }
+
+
+// 3. Display the multiplication tabel with 7
+
+// for (let i = 7; i < 8; i++) {
+//     for (let j = 0; j < 11; j++) {
+//         let product = i * j
+//         document.write(i + " x " + j + " = " + product + "<br>")
+//  }
+    
+// }
+
+// 4. Display the multiplication tables with numbers from 1 to 10
+
+// for (let i = 1; i < 11; i++) {
+//     for (let j = 1; j < 11; j++) {
+//         let product = i * j
+//         document.write(i + " x " + j + " = " + product + "<br>")
+        
+//     }  
+// }
+
+
