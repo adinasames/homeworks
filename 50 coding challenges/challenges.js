d4a9108b31b304fe55d538f0b68a808e229ae1cf
Adina Sames <adina.sames@gmail.com>
2021-02-15 17:30:49 +0200

    tema - 50 coding

--- 50 coding challenges/challenges.js
+++ 50 coding challenges/challenges.js
@@ -33,3 +33,24 @@
 // }
 
 
+// 5. Display the sum of numbers from 1 to 10
+
+// for (let i = 1; i < 11; i++) {
+//     for (let j = 1; j < 11; j++) {
+//         let product = i + j
+//         document.write(i + " + " + j + " = " + product + "<br>")
+        
+//     }  
+// }
+
+// 6. Calculate 10!
+
+
+    var total=1;
+    for (i = 2; i <= 10; i++)
+        total = total * i;
+    document.write(total)
+
+
+
+
