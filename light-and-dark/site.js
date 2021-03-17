f3ad1226b716a33cdc699ae40ba314fb29280bb4
Adina Sames <adina.sames@gmail.com>
2021-03-17 18:41:40 +0200

    css before in dark/light

--- light-and-dark/site.js
+++ light-and-dark/site.js
@@ -7,9 +7,12 @@ setTheme(theme)
 function setTheme(theme) {
     let css = document.querySelector('#darkcss')
     css.disabled = theme == "light" ? true : false
-    let symbol = css.disabled ? "&#9789" : "&#9788"
-    document.querySelector("#switch")
-    .innerHTML = symbol
+
+
+//modificare simbol din JS 
+    // let symbol = css.disabled ? "&#9789" : "&#9788"
+    // document.querySelector("#switch")
+    // .innerHTML = symbol
 
     localStorage.setItem("theme", theme)
 }
